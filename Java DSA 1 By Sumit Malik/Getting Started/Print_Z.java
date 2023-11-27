8bb3096c370cae803f8949568f7ab5c46c43bca1
GunjalkarVijay <152193987+GunjalkarVijay@users.noreply.github.com>
2023-11-27 20:39:52 +0530

    Started 27-11-23

---
+++ Java DSA 1 By Sumit Malik/Getting Started/Print_Z.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Print_Z {
+    public static void main(String[] args){
+        System.out.println("*****");
+        System.out.println("   *");
+        System.out.println("  *");
+        System.out.println(" *");
+        System.out.println("*****");
+    }
+}
